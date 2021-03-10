d0eaee5c2ee9a8301950e94a3ba98679fc2c3b55
Mike Bannoura <mikebannoura@gmail.com>
2021-03-10 03:13:06 -0700

    remove console.log

--- server.js
+++ server.js
@@ -2,17 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log(
-    "got here==>>>111",
-    !req.secure,
-    "proto==>>",
-    req.headers["x-forwarded-proto"] !== "https"
-  );
   if (!req.secure && req.headers["x-forwarded-proto"] !== "https") {
-    console.log("got here==>>>22");
     res.redirect("https://" + req.headers.host + req.url);
   } else {
-    console.log("got here==>>>33");
     next();
   }
 });
